b6e98cdb1578b0662d97730bb6f7886846cb98b5
toughmuffin12 <nelsonc8@spu.edu>
2021-04-05 11:35:42 -0700

    finished app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+1 const http = require('http');
+2
+3 const port = process.env.PORT || 5000;
+4
+5 const server = http.createServer((request, response) => {
+6 	  resonse.statusCode = 200;
+7	  response.setHeader('Content-Type', 'text/plain');
+8         response.end('Hello World\n\nCool!');
+9 });
+10
+11 server.listen(port, (err) => {
+12	   if (err) {
+13		   return console.log('Something went wrong ', err);
+14	   }
+15 	   console.log('Server Listening on: ${port}');
+16 });
